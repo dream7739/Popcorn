9483ec1489cef5c0e8d952e6899ed84706449649
홍정민 <dream7739@naver.com>
2024-10-08 21:38:39 +0900

    feat: baseCollectionViewCell 작성

---
+++ Popcorn/Source/Base/BaseCollectionViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  BaseCollectionViewCell.swift
+//  Popcorn
+//
+//  Created by 홍정민 on 10/8/24.
+//
+
+import UIKit
+
+class BaseCollectionViewCell: UICollectionViewCell {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configureHierarchy()
+        configureLayout()
+        configureView()
+    }
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func configureHierarchy() { }
+    func configureLayout() { }
+    func configureView() { }
+}
