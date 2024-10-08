3f0996df377a0be23a4fb8f3838f874ae031bc0c
seongmin kim <k2417000@naver.com>
2024-10-08 21:44:32 +0900

    feat: Realm Model 생성

--- Popcorn/Source/Database/Model/RealmMovie.swift
+++ Popcorn/Source/Database/Model/RealmMovie.swift
@@ -6,3 +6,20 @@
 //
 
 import Foundation
+import RealmSwift
+
+class RealmMovie: Object {
+    @Persisted(primaryKey: true) var id: Int
+    @Persisted var savedDate: Date
+    
+    @Persisted var name: String
+    @Persisted var videoURL: String
+    
+    convenience init(id: Int, name: String, videoURL: String) {
+        self.init()
+        self.id = id
+        self.savedDate = Date()
+        self.name = name
+        self.videoURL = videoURL
+    }
+}
