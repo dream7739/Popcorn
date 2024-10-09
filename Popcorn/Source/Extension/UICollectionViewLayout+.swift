cf156629f62e5ef290b57c97291fb8a991c51d43
seongmin kim <k2417000@naver.com>
2024-10-09 17:08:22 +0900

    feat: 트렌딩 화면 컬렉션뷰 레이아웃 구현 / 컬렉션뷰 레이아웃 테스트

--- Popcorn/Source/Extension/UICollectionViewLayout+.swift
+++ Popcorn/Source/Extension/UICollectionViewLayout+.swift
@@ -18,6 +18,7 @@ extension UICollectionViewLayout {
         let cellCount: CGFloat = 3
         
         // 셀 사이즈
+        // TODO: - 'main' will be deprecated 대체하기
         let totalWidth = UIScreen.main.bounds.width - 2 * sectionSpacing - (cellCount - 1) * cellSpacing
         let width = totalWidth / cellCount
         let height = width * 1.5
@@ -26,6 +27,24 @@ extension UICollectionViewLayout {
         layout.minimumInteritemSpacing = cellSpacing
         layout.minimumLineSpacing = cellSpacing
         layout.sectionInset = UIEdgeInsets(top: sectionSpacing, left: sectionSpacing, bottom: sectionSpacing, right: sectionSpacing)
+        
+        return layout
+    }
+    
+    // MARK: - 트렌드 화면 - 지금 뜨는 영화, 지금 뜨는 TV 시리즈
+    static func trendLayout() -> UICollectionViewFlowLayout {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .horizontal
+        
+        let cellSpacing: CGFloat = 10
+        
+        // 셀 사이즈
+        let width = 100
+        let height = 150
+        layout.itemSize = CGSize(width: width, height: height)
+        layout.scrollDirection = .horizontal
+        layout.minimumInteritemSpacing = cellSpacing
+        layout.minimumLineSpacing = cellSpacing
         
         return layout
     }
--- Popcorn/Source/Presentation/Trending/TrendingViewController.swift
+++ Popcorn/Source/Presentation/Trending/TrendingViewController.swift
@@ -6,12 +6,77 @@
 //
 
 import UIKit
+import SnapKit
+import Then
 
 final class TrendingViewController: BaseViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
+    private lazy var movieCollectionView = UICollectionView(
+        frame: .zero,
+        collectionViewLayout: .trendLayout()
+    ).then {
+        $0.delegate = self
+        $0.dataSource = self
+        $0.register(
+            SearchCollectionViewCell.self,
+            forCellWithReuseIdentifier: SearchCollectionViewCell.identifier
+        )
     }
     
+    private lazy var seriesCollectionView = UICollectionView(
+        frame: .zero,
+        collectionViewLayout: .trendLayout()
+    ).then {
+        $0.delegate = self
+        $0.dataSource = self
+        $0.register(
+            SearchCollectionViewCell.self,
+            forCellWithReuseIdentifier: SearchCollectionViewCell.identifier
+        )
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func configureHierarchy() {
+        view.addSubview(movieCollectionView)
+        view.addSubview(seriesCollectionView)
+        movieCollectionView.backgroundColor = .blue
+        seriesCollectionView.backgroundColor = .red
+    }
+    
+    override func configureLayout() {
+        movieCollectionView.snp.makeConstraints { make in
+            make.top.horizontalEdges.equalTo(view.safeAreaLayoutGuide)
+            make.height.equalTo(150)
+        }
+        
+        seriesCollectionView.snp.makeConstraints { make in
+            make.top.equalTo(movieCollectionView.snp.bottom).offset(20)
+            make.horizontalEdges.equalTo(view.safeAreaLayoutGuide)
+            make.height.equalTo(150)
+        }
+    }
+}
+
+extension TrendingViewController: UICollectionViewDataSource, UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 30
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(
+            withReuseIdentifier: SearchCollectionViewCell.identifier,
+            for: indexPath
+        ) as? SearchCollectionViewCell else {
+            return UICollectionViewCell()
+        }
+        cell.configureCell(.checkmark)
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        print("셀 탭", indexPath)
+    }
 }
