0ad42dddfbab99fb69265f928ab905fd540701b5
seongmin kim <k2417000@naver.com>
2024-10-10 10:00:04 +0900

    feat: Realm 모델 수정 (isMovie 추가)

--- Popcorn/Source/Database/Model/RealmMovie.swift
+++ Popcorn/Source/Database/Model/RealmMovie.swift
@@ -14,12 +14,14 @@ class RealmMovie: Object {
     
     @Persisted var name: String
     @Persisted var videoURL: String
+    @Persisted var isMovie: Bool
     
-    convenience init(id: Int, name: String, videoURL: String) {
+    convenience init(id: Int, name: String, videoURL: String, isMovie: Bool) {
         self.init()
         self.id = id
         self.savedDate = Date()
         self.name = name
         self.videoURL = videoURL
+        self.isMovie = isMovie
     }
 }
